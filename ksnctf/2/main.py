ecfd5b35501fca48390f5693f7d9383240f5f474
kabik <k2.s1k.x20@gmail.com>
2021-07-26 00:40:26 +0900

    use codecs

--- ksnctf/2/main.py
+++ ksnctf/2/main.py
@@ -1,11 +1,5 @@
-src = 'EBG KVVV vf n fvzcyr yrggre fhofgvghgvba pvcure gung ercynprf n yrggre jvgu gur yrggre KVVV yrggref nsgre vg va gur nycunorg. EBG KVVV vf na rknzcyr bs gur Pnrfne pvcure, qrirybcrq va napvrag Ebzr. Synt vf SYNTFjmtkOWFNZdjkkNH. Vafreg na haqrefpber vzzrqvngryl nsgre SYNT'
+import codecs
 
-for c in src:
-    if c.islower():
-        new_c = chr( ord('a') + (ord(c)-ord('a')+13)%26 )
-    elif c.isupper():
-        new_c = chr( ord('A') + (ord(c)-ord('A')+13)%26 )
-    else:
-        new_c = c
-    print(new_c, end='')
-print()
+print(codecs.decode(
+    'EBG KVVV vf n fvzcyr yrggre fhofgvghgvba pvcure gung ercynprf n yrggre jvgu gur yrggre KVVV yrggref nsgre vg va gur nycunorg. EBG KVVV vf na rknzcyr bs gur Pnrfne pvcure, qrirybcrq va napvrag Ebzr. Synt vf SYNTFjmtkOWFNZdjkkNH. Vafreg na haqrefpber vzzrqvngryl nsgre SYNT',
+    'rot13'))
